25aceb3e01693cd55f18982f2956dcb339488bf7
Voitko Viacheslav <voitko-viacheslav-ivanovich1992@ukr.net>
2023-02-16 19:07:43 +0200

    feat: solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+    let number = 0;
+    if (n < 0) {
+        number -= n;
+    } else {
+        number += n;
+    }
+
+    let str = '' + number;
+
+    let arrayStr = [];
+
+    for(let i = 0; i < str.length; i++) {
+        arrayStr.unshift(str[i]);
+    }
+
+    return Number(arrayStr.join(''));
 }
